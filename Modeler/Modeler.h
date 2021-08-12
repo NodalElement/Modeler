7032d49d5f7056ae6162669186edbd93145a50e0
Pavel <nodalelement@gmail.com>
2021-08-12 09:11:09 +0300

    Initial Commit

---
+++ Modeler/Modeler.h
@@ -0,0 +1,18 @@
+//
+//  Modeler.h
+//  Modeler
+//
+//  Created by Павел Заварин on 12.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Modeler.
+FOUNDATION_EXPORT double ModelerVersionNumber;
+
+//! Project version string for Modeler.
+FOUNDATION_EXPORT const unsigned char ModelerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Modeler/PublicHeader.h>
+
+
